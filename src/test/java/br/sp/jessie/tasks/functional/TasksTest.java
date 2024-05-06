204d8215f8a001d4cf9c5da2b37d9551ae9d9450
antoniojfcloud <antoniojfcloud@outlook.com>
2024-05-06 19:00:45 -0300

    criando testes do selenium-6

--- src/test/java/br/sp/jessie/tasks/functional/TasksTest.java
+++ src/test/java/br/sp/jessie/tasks/functional/TasksTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TasksTest {
 
 	public WebDriver acessarAplicacao(){
-		System.setProperty("webdriver.chrome.driver", "/home/toninho/Cursos/lab-jenkins-udemy/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/tmp/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("http://192.168.15.3:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
